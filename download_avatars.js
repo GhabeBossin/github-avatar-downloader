9302bf73aa54f71265c2350bb5118fb5d790c768
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-15 05:20:43 +0000

    Set up structure in base folder.previous commit pre-emptive, on assumption that project came before assignments in compass

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,6 @@
+// Pre-emptive thinking given the command to run this...
+// var https = require('https');
+// var myArgs = process.argv.slice(2);
+// var repOwner = process.argv.slice[3];
+// var repoName = process.argv.slice[4];
 
-var http = require('http');
-var myArgs = process.argv.slice(2);
-var repOwner = process.argv.slice[3];
-var repoName = process.argv.slice[4];
-
-function requesting(cb) {
-  http.get('https://api.github.com', function(response) {
-    if (response.statusCode !== 200) {
-      cb(new Error('Status code not successful: ' + response.statusCode), null);
-      return;
-    }
-  });
-}
