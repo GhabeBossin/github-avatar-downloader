a79bda1c4a65ba329899fcd423d6180ad57a4ac8
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-15 05:53:36 +0000

    adding getRepoContributors and testing callback

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,4 @@
-// Pre-emptive thinking given the command to run this...
+// Pre-emptive thoughts given the command to run
 // var https = require('https');
 // var myArgs = process.argv.slice(2);
 // var repOwner = process.argv.slice[3];
@@ -8,3 +8,11 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
